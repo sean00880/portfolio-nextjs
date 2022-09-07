ce9ea3b7a68262a5887dbef669fa249302c4aacd
sean00880 <theelitereapersanctum@gmail.com>
2022-09-06 23:46:09 -0400

    layoutfinalize3

--- components/Payments.jsx
+++ components/Payments.jsx
@@ -23,13 +23,13 @@ const Payments = () => {
           </p><hr/>
         </div>
         <div className=' w-[80%] flex-col xl:flex-row  h-auto m-auto shadow-xl shadow-gray-700 rounded-xl flex items-center justify-center p-4 '>
-          <div className='flex leading-20 flex-col border-2 border-black  h-full w-full xl:h-[65vw] 2xl:h-[55vw] xl:w-[23vw] p-5 m-4 text-center text-black hover:scale-105 ease-in duration-300 bg-[#e4e3e3cd]'>
+          <div className='flex leading-20 flex-col border-2 border-black  h-full w-full xl:h-[75vw] 2xl:h-[55vw] xl:w-[23vw] p-5 m-4 text-center text-black hover:scale-105 ease-in duration-300 bg-[#e4e3e3cd]'>
             <h2 className='my-2 mt-auto'>Basic Website Build & Setup</h2><hr/>
             <h4 className='my-2 mt-auto'>Website built and deployed from start to finish with focus on responsiveness, UI design, UX and functionality. Responsive. <div className='bg-black p-4 m-3 text-white rounded-br-[31px] rounded-tl-[31px] rounded-tr-[8px] rounded-bl-[8px] leading-14'><div className='bg-[#e9e9e9] text-black xl:rounded-br-[110px] xl:rounded-tl-[110px] rounded-br-[40px] rounded-tl-[40px] px-3 my-14'> Example Websites:</div> Local Small Business, Salon, Store Front, Simple Blog, Professional Portfolio, SaaS Product Store, etc.</div></h4>
             <h2 className='mt-auto'>$399.99</h2>
             <button className="text-center border-2 bg-[#b13c3c] border-black mt-auto mb-2 bottom-4 h-[5vw] text-lg">Purchase This</button>
           </div>
-          <div className='flex leading-20 flex-col border-2 border-black h-full w-full xl:h-[65vw] xl:w-[23vw] 2xl:h-[55vw] p-5 m-4 text-center text-black hover:scale-105 ease-in duration-300 bg-[#e4e3e3cd] '>
+          <div className='flex leading-20 flex-col border-2 border-black h-full w-full xl:h-[75vw] xl:w-[23vw] 2xl:h-[55vw] p-5 m-4 text-center text-black hover:scale-105 ease-in duration-300 bg-[#e4e3e3cd] '>
             <h2 className='my-2 mt-auto'>Premium Website Build & Setup</h2><hr/>
             <h4 className='my-2 mt-auto'>Multi-Page website built and deployed from start to finish with focus on usability, UI design, UX, functionality, responsiveness and SEO. <br></br>Highly optimized for lead-generation and sales conversion.<div className='bg-black p-4 m-3 text-white rounded-br-[31px] rounded-tl-[31px] rounded-tr-[8px] rounded-bl-[8px]'><div className='bg-[#e9e9e9] text-black xl:rounded-br-[110px] xl:rounded-tl-[110px] rounded-br-[40px] rounded-tl-[40px] px-3 my-14'> Example Websites:</div> Local Business, Handyman Services, Huge Store Fronts, Complex Blogs, Professional Portfolio, SaaS Product Store, eCommerce store, NFT Landing Page.</div></h4>
             <h2 className='mt-auto'>$699.99</h2>
