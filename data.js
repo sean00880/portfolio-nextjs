8776cf2ee9030eb40b05f8aa0452599a1f552af0
sean00880 <theelitereapersanctum@gmail.com>
2022-09-09 17:01:40 -0400

    revision4

--- data.js
+++ data.js
@@ -12,43 +12,53 @@ export const users2 = [
 
 export const faq = [
   {
-    id: 1,
+    id: 5,
     question: "How can I get started with my service?",
     answer: "Start by picking your desired service. Start a quote to get assistance. Pay for the service and your service request will start processing immediately. All files and communication can be directed to developer@decodedweb.us.",
-    id: 2,
+    id: 6,
     question: "What are the costs?",
     answer: "Check 'services' tab for pricing details.",
-    id: 3,
+    id: 4,
     question: "When do you operate?",
     answer: "We operate 24/7 [24 hours 7 days a week].",
-    id: 4,
+    id: 3,
     question: "How long will it take for the website to be setup?",
     answer: "The website will be up and running within 7-21 days.",
-    id: 5,
+    id: 1,
     question: "Where and how will the website be hosted?",
     answer: "Domain and hosting will be covered. The website will be hoested on Github Pages and pushed into Vercel for production deployment.",
-
+    id: 2,
+    question: "How many revisions will I get for my website?",
+    answer: "You will receive 3 revisions with the basic plan and 5 revisions with the premium plan.",
+    id: 7,
+    question: "Where do I exchange files?",
+    answer: "You will be instructed to send your media (text and images) to our business email at developer@decodedweb.us.",
   }
 ];
 
 export const Data = [
   {
-    id: 1,
+    id: 5,
     question: "How can I get started with my service?",
     answer: "Start by picking your desired service. Start a quote to get assistance. Pay for the service and your service request will start processing immediately. All files and communication can be directed to developer@decodedweb.us.",
-    id: 2,
+    id: 6,
     question: "What are the costs?",
     answer: "Check 'services' tab for pricing details.",
-    id: 3,
+    id: 4,
     question: "When do you operate?",
     answer: "We operate 24/7 [24 hours 7 days a week].",
-    id: 4,
+    id: 3,
     question: "How long will it take for the website to be setup?",
     answer: "The website will be up and running within 7-21 days.",
-    id: 5,
+    id: 1,
     question: "Where and how will the website be hosted?",
     answer: "Domain and hosting will be covered. The website will be hoested on Github Pages and pushed into Vercel for production deployment.",
-
+    id: 2,
+    question: "How many revisions will I get for my website?",
+    answer: "You will receive 3 revisions with the basic plan and 5 revisions with the premium plan.",
+    id: 7,
+    question: "Where do I exchange files?",
+    answer: "You will be instructed to send your media (text and images) to our business email at developer@decodedweb.us.",
   }
 ];
 
