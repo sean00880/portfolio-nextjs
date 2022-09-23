8017e7abf382b23c3a8dbcc9a09ee011b75f2d5d
Sean//Decoded// <50418518+sean00880@users.noreply.github.com>
2022-09-23 13:36:53 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,10 @@ import Payments from '../components/Payments'
 import Faq from '../components/Faq'
 import Contact from '../components/Contact'
 
+
+
+export const config = { unstable_runtimeJS: false}
+
 export default function Home() {
   return (
     <div >
