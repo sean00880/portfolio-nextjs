93a22a2225bb2a016b7ec12faead78fbb52bb63e
Sean//Decoded// <50418518+sean00880@users.noreply.github.com>
2022-09-27 19:42:43 -0400

    Update Banner.jsx

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
-import Review from '../public/assets/review.jpg';
-import Website from '../public/assets/website.png';
-import MainLogo from '../public/assets/MainLogo.png';
-import Presence from '../public/assets/Presence.png';
+
 
 
 
@@ -14,7 +11,7 @@ const About = () => {
         <div className='flex'>
         <div className=' md:w-[40%] w-[50%] text-xl'><h2 className='text-center'>What We Offer?</h2><hr/><div className='my-20 indent-8'>Professional web services catered for your custom needs. </div><hr /> <div className='my-20 indent-8'>Solutions that include full support for content creation as well as setup. </div></div>
           <div className='uppercase leading-80 text-xl tracking-widest text-[#eeeeee] md:flex flex-row justify-between relative w-full items-center text-center font-black'>
-            <div className='h-[30vh] sm:w-[60%] w-[30%] mx-3 my-16 lg:my-0'><Image src={Website} width={300} height={300} className='rounded-3xl'></Image><br/> Website Development </div><div className='h-[30vh] w-[30%] sm:w-[60%] mx-3 my-16 lg:my-0' ><Image src={MainLogo} width={300} height={300} className='rounded-3xl'></Image><br/>Digital Design </div><div className='h-[30vh] w-[30%] sm:w-[60%] mx-3 my-16 lg:my-0'><Image src={Presence} width={300} height={300} className='rounded-3xl'></Image><br/>Online Presence Optimization </div>
+            <div className='h-[30vh] sm:w-[60%] w-[30%] mx-3 my-16 lg:my-0'><Image src='https://cdn-icons-png.flaticon.com/512/2888/2888407.png' width={300} height={300} className='rounded-3xl'></Image><br/> Website Development </div><div className='h-[30vh] w-[30%] sm:w-[60%] mx-3 my-16 lg:my-0' ><Image src='https://cdn-icons-png.flaticon.com/512/2888/2888436.png' width={300} height={300} className='rounded-3xl'></Image><br/>Digital Design </div><div className='h-[30vh] w-[30%] sm:w-[60%] mx-3 my-16 lg:my-0'><Image src='https://cdn-icons-png.flaticon.com/512/1458/1458533.png' width={300} height={300} className='rounded-3xl'></Image><br/>Online Presence Optimization </div>
           </div>   
           </div>
           <button className='flex-row w-40 h-20 mx-10'>Services</button>    
