e41533ddf94acf2dab7cfc3c102c425929e0c5ab
Sean//Decoded// <50418518+sean00880@users.noreply.github.com>
2022-09-27 09:47:47 -0400

    Update Main.jsx

--- components/Main.jsx
+++ components/Main.jsx
@@ -11,12 +11,12 @@ const Main = () => {
       <div className='max-w-[1240px] leading-20 pt-40 w-full mx-auto p-2 flex justify-center items-center bg-[#1313139e]'>
         <div >
         <h1 className='py-4 mt-10 pl-7 pr-14 leading-20 text-[#e7e7e7] bg-[#252424] text-center rounded-tl-[110px] rounded-br-[110px]'><span className="text-gray-200 font-bold pb-6">
-							DECODED <span className="text-[#aa283c]">Web</span>
+							DECODED <span className="text-[#aa283c]">Web </span>
 						</span><hr/>Web Development Services</h1>
-        <p className='uppercase text-sm leading-20 tracking-widest bg-[black] text-[#d0d0d0] text-center'>
+        <h3 className='uppercase text-sm leading-20 tracking-widest bg-[black] text-[#d0d0d0] text-center'>
            Digital Design, Web Development, Content Creation and Online Marketing Agency
-          </p>
-          <h2 className='mt-[30vh] mb-[5vh] tracking-widest leading-20 text-[#bd1240] m-auto '>Founded by<span className='text-[#141414]'><GlitchText>Sean Dwivedi</GlitchText></span></h2>
+          </h3>
+          <h2 className='mt-[30vh] mb-[5vh] tracking-widest leading-20 text-[#bd1240] m-auto '>Digital content made by real <span className='text-[#141414]'><GlitchText>Professionals</GlitchText>.</span></h2>
           <div className='p-7 my-6 text-2xl text-black font-bold rounded-xl bg-white'>Need assistance with Web or Web3 services? Check out<span className="text-[#aa283c]"><Link href='/#services' passHref> offered services</Link>.</span> </div>
           <div className='flex items-center justify-between max-w-[330px] m-auto'>
             
