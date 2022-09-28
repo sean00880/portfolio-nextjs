a45784f984cdde27974745272f388189670c1e47
Sean//Decoded// <50418518+sean00880@users.noreply.github.com>
2022-09-28 15:36:37 -0400

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,9 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html>
-      <Script src="https://www.paypal.com/sdk/js?client-id=CLIENT_ID" />
+      
       <Head>
       <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"></link>
       </Head>
