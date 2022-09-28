1902950c0fc789fed07d78a4e853e3db356ea214
Sean//Decoded// <50418518+sean00880@users.noreply.github.com>
2022-09-28 13:52:15 -0400

    Update About.jsx

--- components/About.jsx
+++ components/About.jsx
@@ -33,8 +33,8 @@ const About = () => {
           The focus here at DECODED Web is to provide unique digital solutions that will truly enhance your customers&apos; browsing experience and drastically improve your business or brand&apos;s online presence. We design, develop and create digital content that will highlight your brand in it&apos;s best light. 
           
           </p>
-          <div className='scale-80 w-[40%] h-auto m-auto shadow-lg bg-white shadow-gray-700 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
-          <Image src={PageSpeed} className='rounded-xl' alt='/' priority='true'/>
+          <div className='scale-80 w-[100%] lg:w-[60%] h-auto m-auto shadow-lg bg-white shadow-gray-700 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
+          <Image src={PageSpeed} className='rounded-xl' alt='/' loading='lazy'/>
         </div>
           <p className='mx-10 font-bold indent-7 leading-20 py-2 text-[#f5f5f5] my-4'>
           How are we able to create high converting web applications? We achieve this by adopting and utilizing the ideal technology stack and following best programming practices. Next.JS offers an arsenal of tools, including client-side-rendering as well as server-side rendering, inherent Search Egnine Optimization (SEO), and more. A powerful framework like Next.JS, combined with the vast plethora of libraries and tools available at an angency&apos;s disposal allows for beatiful interfaces to emerge that are packed with all of the bells and whistles needed for a stand-out applications. The inherent SEO features assist with visibility on search engines, making the application suitable for lead generation and customer acquisition.
@@ -48,8 +48,8 @@ Nearly half of consumers (44%) claim to frequently use search engines to learn m
 76% of customers will look up a business&apos;s website before going to one of its physical locations.
 In fact, 45% of customers are likely to visit a business&apos;s physical location after discovering a strong online presence. <br />
 </p>
-<div className='scale-80 w-[40%] h-auto m-auto shadow-lg bg-white shadow-gray-700 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
-          <Image src={Review} className='rounded-xl' alt='/' priority='true'/>
+<div className='scale-80  w-[100%] lg:w-[60%] h-auto m-auto shadow-lg bg-white shadow-gray-700 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
+          <Image src={Review} className='rounded-xl' alt='/'  loading='lazy'/>
         </div>
 <p className='mx-10 font-bold indent-7 leading-20 py-2 text-[#f5f5f5] my-4'>
           
@@ -61,8 +61,8 @@ In fact, 45% of customers are likely to visit a business&apos;s physical locatio
          
           We strive to achieve transformational success for all brands and niches. By utilizing our versatile backgrounds and expertise in various fields that integrate Science, Technology, Engineering, Math (STEM) and art, we build top-of-the line content that boasts originality, excellent performance and professionalism.<br/><br/>Hire us to build your personalized SEO optimzed content today.
           </p>
-          <div className='scale-80 w-[60%] bg-black h-auto m-auto shadow-xl shadow-[white] rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
-          <Image src={AboutImg} className='rounded-xl' alt='/' priority='true'/>
+          <div className='scale-80 w-[100%] lg:w-[60%] bg-black h-auto m-auto shadow-xl shadow-[white] rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
+          <Image src={AboutImg} className='rounded-xl' alt='/' loading='lazy'/>
         </div>
           <Link href='/#projects' passHref>
             <p className='mx-8 indent-2 my-10 py-5 text-[#9f3b3b] text-2xl font-black bg-[#dedede] rounded-3xl underline cursor-pointer text-center'>
