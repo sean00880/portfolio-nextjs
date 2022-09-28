1b03ef94e3a3e477baf5017d2fdda207a4bbf5ff
Sean//Decoded// <50418518+sean00880@users.noreply.github.com>
2022-09-28 16:06:23 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,30 @@
 import dynamic from 'next/dynamic'
 import Head from 'next/head'
-const Banner = dynamic(()=>import('../components/Banner'))
-const About = dynamic(()=>import('../components/About'))
-const Main = dynamic(()=>import('../components/Main'))
-const Skills = dynamic(()=>import('../components/Skills'))
-const Projects = dynamic(()=>import('../components/Projects'))
+const Banner = dynamic(()=>import('../components/Banner'), {
+  ssr: false,
+})
+const About = dynamic(()=>import('../components/About'), {
+  ssr: false,
+})
+const Main = dynamic(()=>import('../components/Main'), {
+  ssr: false,
+})
+const Skills = dynamic(()=>import('../components/Skills'), {
+  ssr: false,
+})
+const Projects = dynamic(()=>import('../components/Projects'), {
+  ssr: false,
+})
 const Faq = dynamic(()=>import('../components/Faq'))
-const Contact = dynamic(()=>import('../components/Contact'))
-const Footer = dynamic(()=>import('../components/Footer'))
-const Navbar = dynamic(()=>import('../components/Navbar'))
+const Contact = dynamic(()=>import('../components/Contact'), {
+  ssr: false,
+})
+const Footer = dynamic(()=>import('../components/Footer'), {
+  ssr: false,
+})
+const Navbar = dynamic(()=>import('../components/Navbar'), {
+  ssr: false,
+})
 
 export default function Home() {
   return (
