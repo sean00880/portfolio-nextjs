8cd0f40da4921451f7223baf08fb80c5ce066b8a
Sean//Decoded// <50418518+sean00880@users.noreply.github.com>
2022-09-28 17:00:09 -0400

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -209,7 +209,7 @@ const Navbar = () => {
                     <AiOutlineMail />
                   </div>
                 </Link>
-                <Link href='/resume' passHref>
+                <Link href='/resume' passHref prefetch='false'>
                   <div
                     onClick={() => setNav(!nav)}
                     className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'
