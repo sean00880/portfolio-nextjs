ccf82e0c8f696b4eef55edacba7c3a38d5bd2513
Sean//Decoded// <50418518+sean00880@users.noreply.github.com>
2022-09-28 21:35:04 -0400

    Update DeveloperTeam.jsx

--- components/DeveloperTeam.jsx
+++ components/DeveloperTeam.jsx
@@ -51,8 +51,8 @@ const Card = ({ project }) => {
                 <div className=''>
                 <span className='text-center text-black bottom-0 absolute p-5 text-sm md:text-lg'><hr/>Links:<br/>
                 <div className='p-3'>
-                <span><a href={project.link} className='inline-flex left-5 absolute'><FaClipboard className='text-sm md:text-md rounded-[xl] mx-3'/>Portfolio</a></span><br/>
-                <span><a href={project.link2} className='inline-flex left-5 absolute'><FaTwitter className='text-sm md:text-md rounded-[xl] mx-3'/>Twitter</a></span><br/>
+                <span><a href={project.link} className='inline-flex left-5 absolute'><Image src='../public/assets/portfolio.png' className='text-sm md:text-md rounded-[xl] mx-3'/>Portfolio</a></span><br/>
+                <span><a href={project.link2} className='inline-flex left-5 absolute'><Image src='../public/assets/social.png' className='text-sm md:text-md rounded-[xl] mx-3'/>Social Media</a></span><br/>
                 </div >
                 Website development for all businesses and niches.</span>
                 
