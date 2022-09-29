08ff9cec21c8ff549564721c70598636dd1ea343
Sean//Decoded// <50418518+sean00880@users.noreply.github.com>
2022-09-28 21:51:52 -0400

    Update MarketingTeam.jsx

--- components/MarketingTeam.jsx
+++ components/MarketingTeam.jsx
@@ -49,8 +49,8 @@ const Card = ({ project }) => {
                 <div className=''>
                 <span className='text-center text-black bottom-0 absolute p-5 text-sm md:text-lg'><hr/>Links:<br/>
                 <div className='p-3'>
-                <span><a href={project.link} className='inline-flex left-5 absolute'><Image src='../public/assets/portfolio.png' className='text-sm md:text-md rounded-[xl] mx-3'/>Portfolio</a></span><br/>
-                <span><a href={project.link2} className='inline-flex left-5 absolute'><Image width="200" height="200" src='../public/assets/social.png' className='text-sm md:text-md rounded-[xl] mx-3'/>Social Media</a></span><br/>
+                <span><a href={project.link} className='inline-flex left-5 absolute'><Image src='/../public/assets/portfolio.png' className='text-sm md:text-md rounded-[xl] mx-3'/>Portfolio</a></span><br/>
+                <span><a href={project.link2} className='inline-flex left-5 absolute'><Image width="200" height="200" src='/../public/assets/social.png' className='text-sm md:text-md rounded-[xl] mx-3'/>Social Media</a></span><br/>
                 </div >
                 Website development for all businesses and niches.</span>
                 
