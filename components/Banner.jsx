a6be28f60a5991c833415ec477836999462ddb95
Sean//Decoded// <50418518+sean00880@users.noreply.github.com>
2022-10-06 18:04:39 -0400

    Update Banner.jsx

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export const config = {
   unstable_runtimeJS: false
@@ -12,10 +13,10 @@ const About = () => {
         <div className='flex'>
         <div className=' md:w-[40%] w-[50%] m-6 p-6 text-xl bg-white text-black rounded-2xl font-black'><h2 className='text-center'>What We Offer?</h2><hr/><div className='my-20 indent-8'>Professional web services catered for your custom needs. </div><hr /> <div className='my-20 indent-8'>Solutions that include full support for content creation as well as setup. </div></div>
           <div className='uppercase leading-80 text-xl tracking-widest text-[#eeeeee] md:flex flex-row justify-between relative w-full items-center text-center font-black'>
-            <div className='h-[30vh] sm:w-[60%] w-[77%] mx-3 my-24 text-sm lg:text-xl lg:my-0'><Image src='https://i.ibb.co/gdVhdNj/image1.png' width={300} height={300} className='rounded-3xl'/><br/> Website Development <hr />- Websites with focus on UI, UX and SEO. </div><div className='h-[30vh] w-[77%] sm:w-[60%] mx-3 my-24 lg:my-0 text-sm lg:text-xl' ><Image src='https://i.ibb.co/3m0cVMd/image2.png' width={300} height={300} className='rounded-3xl'/><br/>Digital Design </div><div className='h-[30vh] w-[77%] sm:w-[60%] mx-3 my-24 lg:my-0 text-sm lg:text-xl'><Image src='https://i.ibb.co/NKT8mWy/image3.png' width={300} height={300} className='rounded-3xl'/><br/>Online Presence Optimization </div>
+            <div className='h-[30vh] sm:w-[60%] w-[77%] mx-3 my-24 text-sm lg:text-md lg:my-0'><Image src='https://i.ibb.co/gdVhdNj/image1.png' width={300} height={300} className='rounded-3xl'/><br/> Website Development <hr />- Websites with prioritization on lead generation via focus on UI, UX and functionality. </div><div className='h-[30vh] w-[77%] sm:w-[60%] mx-3 my-24 lg:my-0 text-sm lg:text-md' ><Image src='https://i.ibb.co/3m0cVMd/image2.png' width={300} height={300} className='rounded-3xl'/><br/>Digital Design <hr />- Logo Design, Graphic Design & Branding.</div><div className='h-[30vh] w-[77%] sm:w-[60%] mx-3 my-24 lg:my-0 text-sm lg:text-md'><Image src='https://i.ibb.co/NKT8mWy/image3.png' width={300} height={300} className='rounded-3xl'/><br/>Online Presence Optimization<hr />- Content Setup & Management, Search Engine Optimization. </div>
           </div>   
           </div>
-          <button className='flex-row w-full h-20 100vh border-4'>View Our Services</button>    
+          <Link href='/services' passhref><button className='flex-row w-full h-20 100vh border-4'>View Our Services</button></Link> 
         </div>
     </div>
   );
