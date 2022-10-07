58d0ace23dd108b199cf21c92c4ece7a87cb7a47
Sean//Decoded// <50418518+sean00880@users.noreply.github.com>
2022-10-06 23:55:39 -0400

    Update Skills.jsx

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -17,13 +17,13 @@ import GlitchText from "./glitch-text";
 
 const Skills = () => {
   return (
-    <div id='skills' className='w-full h-auto p-2 py-16 bg-[#f1f1f1] bg-bg_image bg-cover bg-fixed'>
-      <div className='max-w-[1240px] p-2 py-20 mx-auto flex flex-col justify-center rounded-tr-[10%] rounded-bl-[10%] border-[#b11b3498] border-double border-4 rounded-sm bg-[#00000090]' >
+    <div id='skills' className='w-full h-auto p-2 bg-[#f1f1f1] bg-bg_image bg-cover bg-fixed'>
+      <div className='max-w-[1240px] p-2 py-20 mx-auto flex flex-col justify-center rounded-tr-[10%] rounded-bl-[10%] border-[#b11b3498] border-double border-4 rounded-sm bg-bg_image12 bg-cover bg-fixed' >
       <div className='uppercase text-xl text-center tracking-widest text-[#f4f4f4]'>
           <h2><GlitchText>Stack</GlitchText></h2>
           <hr></hr>
           </div>
-        <h2 className='py-4 text-center'>Technologies Deployed:</h2>
+        <h2 className='py-4 text-center'>Technologies Deployed</h2>
         <div className='grid p-2 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8'>
           <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300  bg-white text-red-600 border-[.3rem] border-inset border-[#131212de] ring-[#6464649c]'>
             <div className='grid gap-4 justify-center items-center'>
