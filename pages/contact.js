db48d19ce92cd64df042e05235f42d8172d3d26d
Sean//Decoded// <50418518+sean00880@users.noreply.github.com>
2022-10-14 13:18:25 -0400

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -5,24 +5,10 @@ import Popup from "../components/Popup.js";
 import GlitchText from "../components/glitch-text5";
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import Contact from '../components/Contact'
 import Head from 'next/head'
 
 
-const contact = () => {
-    const form = useRef();
-  
-    const sendEmail = (e) => {
-      e.preventDefault();
-  
-      emailjs.sendForm('service_dvckjdg', 'template_3cs9fmt', form.current, 'jUn1z-vodKfQLijZ7')
-        .then((result) => {
-            console.log(result.text);
-        }, (error) => {
-            console.log(error.text);
-        });
-    };
-  
-  const [buttonPopup, setButtonPopup] = useState(false);
 
   return (
     <>
@@ -35,35 +21,7 @@ const contact = () => {
         </head>
       </Head>
       <Navbar />
-    <div id='contact' className='pt-60 w-full pb-[20px] bg-[#f1f1f1] bg-bg_image11 bg-contain bg-center bg-no-repeat bg-fixed'>
-      <div className='max-w-[1240px] py-20 mx-auto flex flex-col justify-center bg-[#c7b7baf7] border-double border-2 rounded-sm border-gray-800' >
-      <div className='uppercase text-xl text-center tracking-widest text-[#ce3636]'>
-          <h2><GlitchText>Contact</GlitchText></h2>
-          <hr></hr>
-          </div>
-          <h2 className='py-4 mt-4 text-center text-[#638d5e] bg-[#1a1a1a] align-left w-full rounded-tr-border-2 px-10 rounded-br-[10px]  rounded-tl-[10px]'>Submit a Quote Request/Connect with Us</h2>
-      
-      <form ref={form} onSubmit={sendEmail} className='flex-col'>
-        <div className='m-2 flex flex-col'>
-        <input className='my-2 h-10 rounded-sm border-2 border-black' type="text" placeholder="Name" name="name"/>
-        <input className='my-2 h-10 rounded-sm border-2 border-black' type="text" placeholder="Phone" name="contact" />
-        <input className='my-2 h-10 rounded-sm border-2 border-black' type="email" placeholder="Email" name="email" />
-        <input className='my-2 h-10 rounded-sm border-2 border-black' type="text" placeholder="Subject" name="subject"/>
-        <textarea
-          className='border-2 border-[#aa283c]'
-          type="text"
-          rows={6}
-          placeholder="Message"
-          name="message"
-        />
-        </div>
-        <button className='text-center justify-center w-full h-10' onClick={()=>setButtonPopup(true)}>SUBMIT</button>
-        <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
-          <h3>Your message has been sent. We will reach out to you shortly.</h3>
-        </Popup>
-        </form>
-    </div>
-    </div>
+    <Contact />
     <Footer />
     </>
   );
