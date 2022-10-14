31db4c50801ccf14ef8223441562f02214615031
Sean//Decoded// <50418518+sean00880@users.noreply.github.com>
2022-10-14 13:10:47 -0400

    Update Card2.jsx

--- components/Card2.jsx
+++ components/Card2.jsx
@@ -8,7 +8,7 @@ const Card2 = () => {
     <h2 className='my-2 mt-auto'>Test Pay</h2><hr/>
     <h4 className='my-2 mt-auto'>Multi-Page website built and deployed from start to finish with focus on usability, UI design, UX, functionality, responsiveness and SEO.
 Highly optimized for lead-generation and sales conversion.<div className='bg-black p-4 m-3 text-white rounded-br-[31px] rounded-tl-[31px] rounded-tr-[8px] rounded-bl-[8px] leading-14'><div className='bg-[#e9e9e9] text-black xl:rounded-br-[110px] xl:rounded-tl-[110px] rounded-br-[40px] rounded-tl-[40px] px-3 my-14'> Example Websites:</div> Local Business, Handyman Services, Huge Store Fronts, Complex Blogs, Professional Portfolio, SaaS Product Store, eCommerce store, NFT Landing Page.</div></h4>
-    <h2 className='mt-auto'>$4.99</h2>
+    <h2 className='mt-auto'>$699.99</h2>
     
        <Link href='/contact' passhref>
       <button className='bg-[#d7d7d776] mt-auto p-2 rounded-md z-0 relative'>Message Us</button>
