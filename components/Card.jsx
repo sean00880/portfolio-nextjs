19a82cc1c70d15b666b5cb1f06667b4547a75251
Sean//Decoded// <50418518+sean00880@users.noreply.github.com>
2022-10-14 12:47:16 -0400

    Update Card.jsx

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,5 +1,5 @@
 
-import {PayPalScriptProvider, PayPalButtons} from "@paypal/react-paypal-js"
+import Link from 'next/link'
 import React from 'react'
 
 const Card = () => {
@@ -8,31 +8,9 @@ const Card = () => {
     <h2 className='my-2 mt-auto'>Website Build & Setup</h2><hr/>
     <h4 className='my-2 mt-auto'>Website built and deployed from start to finish with focus on responsiveness, UI design, UX and functionality. Responsive. <div className='bg-black p-4 m-3 text-white rounded-br-[31px] rounded-tl-[31px] rounded-tr-[8px] rounded-bl-[8px] leading-14'><div className='bg-[#e9e9e9] text-black xl:rounded-br-[110px] xl:rounded-tl-[110px] rounded-br-[40px] rounded-tl-[40px] px-3 my-14'> Example Websites:</div> Local Small Business, Salon, Store Front, Simple Blog, Professional Portfolio, SaaS Product Store, etc.</div></h4>
     <h2 className='mt-auto'>$399.99</h2>
-    
-    <PayPalScriptProvider
-      options={{
-        "client-id": "AbUS3UDeVlG3e5JvSBPx2iCVL81biUiZCaQymapp43Di1Y8npVEi1WZW5w0L2lE9_bR8rf2h_lm5uPmC"
-      }}>
-      <PayPalButtons className='bg-[#d7d7d776] mt-auto p-2 rounded-md z-0 relative' createOrder={(data, actions) => {
-        return actions.order
-            .create({
-                purchase_units: [
-                    {
-                        amount: {
-                            value: "399.99",
-                        },
-                    },
-                ],
-            })
-    }}
-    onApprove={async (data, actions) => {
-            const details = await actions.order.capture();
-            const name = details.payer.name.given_name;
-            alert("Transaction completed by " + name);
-          }}
-    />
-    </PayPalScriptProvider>
-    
+    <Link href='/contact' passhref>
+      <button className='bg-[#d7d7d776] mt-auto p-2 rounded-md z-0 relative'>Message Us</button>
+    </Link>
   </div>
   );
 };
