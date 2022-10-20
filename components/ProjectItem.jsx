438017d7a10a81a26f285ee72ff8fbff320d23ce
Sean//Decoded// <50418518+sean00880@users.noreply.github.com>
2022-10-20 17:53:02 -0400

    Update ProjectItem.jsx

--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -1,13 +1,21 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import Logo from '../public/assets/logo2.png'
 
 
 const ProjectItem = ({title, backgroundImg, tech, projectUrl}) => {
   return (
     <div className='relative flex items-center justify-center border-4 border-inset border-[#ffffffe9] bg-[#000000f1] h-[50vh] w-full shadow-xl shadow-[#d9d9d9] rounded-xl group hover:bg-gradient-to-r from-[#e8e8ee] to-[#991830] '>
     <Image className='rounded-xl group-hover:opacity-10' src={backgroundImg} alt='/' loading='lazy'/>  <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]  bg-black rounded-2xl p-4 border-4 border-double border-[#ffffffe5]'>
-
+    <Image
+              src={Logo}
+              alt='/'
+              width='100px'
+              height= '90px'
+              className='cursor-pointer self-baseline items-baseline align-baseline r-[10px] absolute'
+              priority='true'
+            />
         <h3 className='text-xl text-[#f5f5f5] font-bold tracking-wider text-center'>{title}</h3>
         <hr />
         <p className='pb-2 pt-2 text-[#fdfdfd] text-center'>{tech}</p>
