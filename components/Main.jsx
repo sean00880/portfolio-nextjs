b47c87e100f3cd2a57fd83da46fd2a2ebc40e6f0
Sean//Decoded// <50418518+sean00880@users.noreply.github.com>
2022-10-22 14:37:22 -0400

    Update Main.jsx

--- components/Main.jsx
+++ components/Main.jsx
@@ -13,7 +13,7 @@ const Main = () => {
   return (
     <div id='home' className='pb-10 w-full h-auto xl:h-[100vh] text-center bg-bg_image10 lg:bg-bg_image2 bg-cover bg-fixed border-b-4'>
       <div className='px-3 justify-around leading-20 pt-40 md:pt-30 w-full mx-auto p-2 flex flex-col xl:flex-row items-center bg-[#1313139e]'>
-      <div className='h-[100vh] lg:h-auto'>
+      <div className='h-auto'>
       <h1 className='py-2 mt-10 pl-14 pr-14 leading-20 text-3xl text-[#e7e7e7] bg-[#252424]  text-center rounded-tl-[110px] rounded-br-[110px]'><span className="text-gray-200 font-bold pb-3">
 							DECODED <span className="text-[#aa283c]">Web </span>
 						</span><hr/>Web Development Services</h1>
