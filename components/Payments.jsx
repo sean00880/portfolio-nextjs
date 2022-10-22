0d903c7371d5398fb290b8d7224d856fa87ed5cf
Sean//Decoded// <50418518+sean00880@users.noreply.github.com>
2022-10-22 15:03:49 -0400

    Update Payments.jsx

--- components/Payments.jsx
+++ components/Payments.jsx
@@ -6,7 +6,7 @@ import Card from './Card'
 import Card2 from './Card2'
 import Contact from './Contact2';
 import Link from 'next/link'
-import Image from 'next/image'
+import Image from 'next/Image'
 import Logo from '../public/assets/logo2.png'
 
 // Make sure to call `loadStripe` outside of a component’s render to avoid
@@ -38,18 +38,18 @@ const Payments = () => {
           </div>
       <div id='banner' className=' relative w-full items-center py-16 bg-[#ffffffd4] rounded-2xl border-4 my-3'>
                 
-                <div className='py-10 w-full relative flex flex-col m-auto lg:flex-row bg-[#0e0d0de9] rounded-sm border-red px-5 border-t-2 border-b-2 border-white'>
+                <div className='py-10 w-full relative flex flex-col m-auto lg:flex-row bg-[#0e0d0de9] rounded-sm border-red border-t-2 border-b-2 border-white'>
                  
-                <div className='bg-bg_ad bg-cover bg-fixed bg-no-repeat w-full lg:w-[30%] mx-2 rounded-2xl p-4 bg-[#181a1882] border-2 border-double'><div className='bg-[#000000e3] w-full h-full text-center rounded-2xl border-double border-4 p-6'> <h2 className='text-center text-xl font-[900] text-[#549751] p-6 leading-20 shimmer'> Local Business? <hr /> Get an Enterprise Website Developed & Setup for only $99 down!</h2><br /><button className='p-3 m-3 border-4 '>Contact Us</button></div></div>
+                <div className='bg-bg_ad bg-cover bg-fixed bg-no-repeat w-full lg:w-[30%] rounded-2xl p-4 bg-[#181a1882] border-2 border-double'><div className='bg-[#000000e3] w-full h-full text-center rounded-2xl border-double border-4 p-6'> <h2 className='text-center text-xl font-[900] text-[#549751] p-6 leading-20 shimmer'> Local Business? <hr /> Get an Enterprise Website Developed & Setup for only $99 down!</h2><br /><button className='p-3 m-3 border-4 '>Contact Us</button></div></div>
                  
-                 <div className="flex-col bg-[#ffffffd4] border-2 border-[#d3d0d0] p-3 rounded-2xl">
+                 <div className="flex-col bg-[#ffffffd4] border-2 border-[#d3d0d0] my-2 p-3 rounded-2xl">
                   
-                  <div className='flex'>
-                  <div className=' md:w-[40%] w-[50%] my-6 p-6 text-xl border-4 border-double border-black bg-white text-black rounded-2xl font-black'><h2 className='text-center'>What We Offer?</h2><hr/><div className='my-20 indent-8'>Professional web services catered for your custom needs. </div><hr /> <div className='my-20 indent-8'>Solutions that include full support for content creation as well as setup. </div></div>
-                    <div className='uppercase leading-80 text-xl tracking-widest text-[#8b2a2a] md:flex flex-row justify-between relative w-full items-center text-center font-black'>
-                      <div className=' w-full mx-3 py-10 text-sm lg:text-md lg:my-0'><Image src='https://i.ibb.co/gdVhdNj/image1.png' width={300} height={300} className='rounded-3xl'/><br/> Website Development <hr />- Websites with prioritization on lead generation via focus on UI, UX and functionality. </div><div className=' w-full mx-3 py-10 lg:my-0 text-sm lg:text-md' ><Image src='https://i.ibb.co/3m0cVMd/image2.png' width={300} height={300} className='rounded-3xl'/><br/>Digital Design <hr />- Logo Design, Graphic Design & Branding.</div><div className=' w-full mx-3 py-10 lg:my-0 text-sm lg:text-md'><Image src='https://i.ibb.co/NKT8mWy/image3.png' width={300} height={300} className='rounded-3xl'/><br/>Online Presence Optimization<hr />- Content Setup & Management, Search Engine Optimization. </div>
-                    </div>   
-                    </div>
+                 <div className='flex'>
+        <div className=' md:w-[40%] w-[50%] my-6 p-2 text-lg border-4 border-double border-black bg-white text-black rounded-2xl font-black'><h2 className='text-center'>What We Offer?</h2><hr/><div className='my-20 indent-3 lg:indent-6'>Professional web services catered for your custom needs. </div><hr /> <div className='my-20 indent-8'>Solutions that include full support for content creation as well as setup. </div></div>
+          <div className='uppercase leading-80 text-xl tracking-widest text-[#7e3333] md:flex flex-row justify-between relative w-full items-center text-center font-black'>
+            <div className=' mx-3 py-4 text-sm lg:text-md lg:my-0'><Image src='https://i.ibb.co/gdVhdNj/image1.png' width={300} height={300} className='rounded-3xl'/><br/> Website Development <hr className='text-black bg-black' />- Websites with prioritization on lead generation via focus on UI, UX and functionality. </div><div className='  mx-3 py-4 lg:my-0 text-sm lg:text-md' ><Image src='https://i.ibb.co/3m0cVMd/image2.png' width={300} height={300} className='rounded-3xl'/><br/>Digital Design <hr />- Logo Design, Graphic Design & Branding.</div><div className=' mx-3 py-4 lg:my-0 text-sm lg:text-md'><Image src='https://i.ibb.co/NKT8mWy/image3.png' width={300} height={300} className='rounded-3xl'/><br/>Online Presence Optimization<hr />- Content Setup & Management, Search Engine Optimization. </div>
+          </div>   
+          </div>
                     </div>
                   </div>
                   <div className="flex-col w-auto h-auto bg-white text-center m-6 p-2 rounded-2xl bg-bg_ad2 bg-fixed"><h2 className='text-gray-600 bg-[#ffffffdf] text-center text-[1.2rem] rounded-2xl tracking-widest leading-[2.4em] border-4 border-double border-black'> <span className='text-[#fdc140] styledtitle'> HALLOWEEN PROMO </span><hr /> <h5 className='text-[1rem] font-extrabold text-red-800'>Valid till November 3rd 2022</h5> Operating a <span className='text-[green]'>Landscaping Business</span>? Professional, high converting websites for only $399.99!</h2></div>
