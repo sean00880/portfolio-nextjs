0cbe0fcf941f6fe1614bc12bd5f3fe72607a5abf
Sean//Decoded// <50418518+sean00880@users.noreply.github.com>
2022-10-28 15:45:01 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
         <meta name="description" content="Websites and digital services for all brands and niches | Websites, digital design, marketing & more." />
         <meta name="google-site-verification" content="HaKP2hPcnleHL77Ut6RaNNEYiCsnZOWOTnAuZ3ZyXaE" />
         <link rel="icon" href="/favicon.ico" />
-     <script src="https://sitesjs.gosite.com/messenger/global-chat.min.js" owd="52dd819272722a723611b454a5e14ccbe349d6e4d5be8d3c" async>
+     <script src="https://sitesjs.gosite.com/messenger/global-chat.min.js" owd="52dd819272722a723611b454a5e14ccbe349d6e4d5be8d3c" async/>
       </Head>
       <Navbar />
       <Main />
