e25b1f81724dcb6a3b0421ba54cb34e707f94df4
Sean//Decoded// <50418518+sean00880@users.noreply.github.com>
2022-10-27 22:27:12 -0400

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,7 @@ export default function Document() {
     <Html>
       
       <Head>
+    <script src="https://sitesjs.gosite.com/messenger/global-chat.min.js" owd="52dd819272722a723611b454a5e14ccbe349d6e4d5be8d3c"></script>
       <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"></link>
     <meta name="google-site-verification" content="HaKP2hPcnleHL77Ut6RaNNEYiCsnZOWOTnAuZ3ZyXaE" />
       </Head>
