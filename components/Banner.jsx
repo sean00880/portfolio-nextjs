9cf1b8021d7e2e8aa04ea86834be258e8910e070
Sean//Decoded// <50418518+sean00880@users.noreply.github.com>
2022-10-28 21:51:39 -0400

    Update Banner.jsx

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -71,7 +71,7 @@ const About = () => {
         <hr />
         <div className='m-6'>
         <h2 className='text-[21px] text-center p-8'>Professional custom websites fully optimized available at affordable rates!</h2>
-        <h4 className="bg-white text-center text-red-700 p-12 mx-6 rounded-xl ">
+        <h4 className="bg-white text-center text-[#2d1d1d] p-12 mx-6 rounded-xl ">
           Operating a business website in the United States can cost between $10,000 - $25,000. According to various sources, setting up a business website alone costs $3,500 on average. <br /> <br />
           While a lot of manpower is involved in website development, we believe website solutions should be accessible to most business owners at affordable rates. <br /><br />
           That is why, at Decoded Web, we pledge to charge low set-up costs for fully optimized web solutions so you can allocate your budget on marketing and other business-related needs. <br /><br />
