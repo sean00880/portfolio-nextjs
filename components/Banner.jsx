99c5e6694c2a96f4c7b667cbae18f349666d1533
Sean//Decoded// <50418518+sean00880@users.noreply.github.com>
2022-10-28 21:16:54 -0400

    Update Banner.jsx

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -29,7 +29,7 @@ const About = () => {
        
       <div className='bg-bg_ad bg-cover bg-fixed bg-no-repeat w-auto lg:w-[30%] rounded-2xl p-4 bg-[#181a1882] border-2 border-double'><div className='bg-[#000000e3] w-full h-full text-center rounded-2xl border-double border-4 p-6'> <h2 className='text-center text-xl font-[900] text-[#549751] p-6 leading-20 shimmer'> Local Business? <hr /> Get an Enterprise Website Developed & Setup for only $99 down!</h2><br /><button className='p-3 m-3 border-4 '>Contact Us</button></div></div>
        
-       <div className="flex-col bg-[#f7f7f7] p-[33px] rounded-[13px] m-5">
+       <div className="flex flex-col items-center bg-[#f7f7f7] p-[33px] rounded-[13px] m-5">
         
         <div className='flex'>
         <div className=' md:w-[40%] w-[50%] my-6 p-6 text-xl border-4 border-double border-black bg-white text-black rounded-2xl font-black'><h2 className='text-center'>What We Offer?</h2><hr/><div className='my-20 indent-8'>Professional web services catered for your custom needs. </div><hr /> <div className='my-20 indent-8'>Solutions that include full support for content creation as well as setup. </div></div>
@@ -37,7 +37,7 @@ const About = () => {
             <div className=' mx-3 py-4 text-sm lg:text-md lg:my-0 w-full'><Image src='https://i.ibb.co/gdVhdNj/image1.png' width={300} height={300} className='rounded-3xl'/><br/> Website Development <hr />- Websites with prioritization on lead generation via focus on UI, UX and functionality. </div><div className='  mx-3 w-full py-4 lg:my-0 text-sm lg:text-md' ><Image src='https://i.ibb.co/3m0cVMd/image2.png' width={300} height={300} className='rounded-3xl'/><br/>Digital Design <hr />- Logo Design, Graphic Design & Branding.</div><div className=' mx-3 py-4 lg:my-0 text-sm w-full lg:text-md'><Image src='https://i.ibb.co/NKT8mWy/image3.png' width={300} height={300} className='rounded-3xl'/><br/>Online Presence Optimization<hr />- Content Setup & Management, Search Engine Optimization. </div>
           </div>   
           </div>
-          <Link href='/services' passhref><button className='flex-row w-full h-20 100vh border-4'>List of Services</button></Link> 
+          <Link href='/services' passhref><button className='flex-row h-20 100vh border-4'>List of Services</button></Link> 
           </div>
          
         </div>
