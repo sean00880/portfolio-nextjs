1c8ec9e3189e2226c364d25e1b43a09c2857fb12
Sean//Decoded// <50418518+sean00880@users.noreply.github.com>
2022-12-02 12:32:39 -0500

    Update BannerCards.jsx

--- components/BannerCards.jsx
+++ components/BannerCards.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "React";
+import React, { useState } from "react";
 import ReactCardFlip from "react-card-flip";
 import Data from "./bannerData.json";
 import team from "../pages/team";
